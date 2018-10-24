abb11dc3687e84195fc8427308a7c495c15f5c20
jmsinusa <jmsinusa@gmail.com>
2018-10-24 09:24:38 -0600

    Pre-processing Chunk boiler plate code

--- pre_processing.py
+++ pre_processing.py
@@ -5,7 +5,6 @@ Load raw data into numpy
 import numpy as np
 import pandas as pd
 from datetime import datetime
-import csv
 
 
 def load_raw_spreadsheet(filepath):
@@ -41,13 +40,22 @@ def load_raw_spreadsheet(filepath):
     elapsed_time = np.array(elapsed_time.values)
     time_arr = np.array(df['time'].values)
     series = {'true_time': time_arr, 'elapsed_time': elapsed_time, 'cols': list(df.columns[1:-2])}
-    series['sensor_vals'] = np.array(df.iloc[:,1:-2].values)
+    series['sensor_vals'] = np.array(df.iloc[:,1:-2].values).T
     series['swim_class'] = np.array(df['Swim class'].values)
     series['swim_class_def'] = {1: 'Not swimming', 2: 'Swimming', 3: 'Push off event', 4: 'Turn event'}
     series['stroke_phase_def'] = {0: 'No event', 1: 'R Hand Entry', 2: 'R Start of Down Sweep', 3: 'R Catch', 4: 'R Recovery',
                                   5: 'L Hand Entry', 6: 'L Start of Down Sweep', 7: 'L Catch', 8: 'L Recovery'}
     series['stroke_phase'] = np.array(df['Stroke class'].values, dtype=np.int8) # Note: This forces null to 0.
     series['subject_cat'] = df.iloc[0][1]
+
+    # Normalise sensor_vals to mean=0 std=1
+    vals = series['sensor_vals']
+    means = np.mean(vals, axis=1)
+    means2 = np.tile(np.expand_dims(means, 1), (1, vals.shape[1]))
+    stds = np.std(vals, axis=1)
+    std2 = np.tile(np.expand_dims(stds, 1), (1, vals.shape[1]))
+    newvals = (vals - means2) / std2
+    series['sensor_vals_normed'] = newvals
     return series
 
 
@@ -79,6 +87,25 @@ def verify_spreadsheet(filepath):
     assert int(df.iloc[7][15]) == 8
     assert df.iloc[0][16] == 'R Hand Entry'
     assert df.iloc[4][16] == 'L Hand Entry'
+
+
+def chunk_series(series, sentence_length=120):
+    """
+    Split series data into a number of events, each of length sentence_length
+    :param series:
+    :return: data, dict containing:
+        elapsed_time np.array[chunk no, time step]) = elapsed time FROM START OF CHUNK in seconds
+        sensor_vals_normed: np.array([chunk no, sensor no, timestep]) = sensor val
+        swim_class: np.array([chunk no, time step]) = swim class
+        stroke_phase: np.array([chunk no, time step]) = Stroke events. Most are zero (null). Others are 1 - 8.
+        # And the following, for reference.
+        cols: Name of the 12 columns that make up the sensor vals
+        swim_class_def: Definitions for the four swim classes
+        stroke_phase_def: Stroke event defintions.
+        subject_cat: Category of subject (Elite)
+
+    """
+    raise NotImplementedError('Chuck series not yet created.')
 
 def _format_time(time_str):
     return datetime.strptime(time_str, "'%H:%M:%S.%f'")
